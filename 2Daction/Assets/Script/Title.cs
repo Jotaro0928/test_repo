9cb8708924167144d029225deb2da8e557470357
Jotaro0928 <jojo4dorarara@gmail.com>
2020-12-15 13:35:56 +0900

    2Daction_backup
    2Dアクションゲームのバックアップ

---
+++ 2Daction/Assets/Script/Title.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+    private bool firstPush = false;
+    //スタート」ボタンを押されたら呼ばれる
+    public void PressStart()
+    {
+        Debug.Log("Press Start!");
+        if (!firstPush)
+        {
+            Debug.Log("Go Next Scene!");
+            //ここに次のシーンへいく命令を書く
+
+            //
+            firstPush = true;
+        }
+    }
+
+}
